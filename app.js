18ad8b85d6b5d01b6edceebbb29766b085fe156e
NathanielPicard-Busky <npb244@nyu.edu>
2017-11-14 17:11:17 -0500

    modify listen-on port
    app.js line 20 and 270
    copied from getting started

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ var Location = mongoose.model('Location');
 var Review = mongoose.model('Review');
 
 app.set('view engine', 'hbs');
-
+app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(publicPath));
 
@@ -267,4 +267,7 @@ app.post('/review', function(req, res){
 
 
 
-app.listen(8080, '127.0.0.1');
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
+//app.listen(8080, '127.0.0.1');
